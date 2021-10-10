1455f378b60e1c223b80911bb27d14a22237d659
vjacheslav28 <perever.vjachesla@gmail.com>
2021-10-10 22:46:04 +0300

    init commit

--- src/com/pb/pereverzev/hw1/HelloWorld.java
+++ src/com/pb/pereverzev/hw1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.pb.pereverzev.hw1;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World 2021");
+        System.out.println("Hello World 2021 22:43");
    }
 }
