e05626d23d02630417c1d9f4e46f04a287dcc80c
vjacheslav28 <perever.vjachesla@gmail.com>
2021-10-30 17:06:14 +0300

    init commit

---
+++ src/com/pb/pereverzev/hw5/Book.java
@@ -0,0 +1,9 @@
+package com.pb.pereverzev.hw5;
+
+public class Book {
+    public static void{
+        String name;        //название
+        String autorBook;   //автор книги
+        int yearPublishing; // год издания
+    }
+}
---
+++ src/com/pb/pereverzev/hw5/Library.java
@@ -0,0 +1,7 @@
+package com.pb.pereverzev.hw5;
+import utilBook.*;
+public class Library {
+    public static void main(String[] args){
+        .
+    }
+}
