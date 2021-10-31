259db5fa1d78f0955aefd9184ab0bad9dcea850c
vjacheslav28 <perever.vjachesla@gmail.com>
2021-11-01 01:15:07 +0200

    init commit

--- src/com/pb/pereverzev/hw5/Library.java
+++ src/com/pb/pereverzev/hw5/Library.java
@@ -7,15 +7,15 @@ public class Library {
         Book book1 =new Book();
         Book book2 =new Book();
         Book book3 =new Book();
-        book1.getAutorBook("Иванов И. И.");
-        book1.getName("Приключения");
-        book1.getYearPublishing(2000);
-        book2.getAutorBook("Сидоров А. В");
-        book2.getName("Словарь");
-        book2.getYearPublishing(1980);
-        book3.getAutorBook("Гусев К. В.");
-        book3.getName("Энциклопедия");
-        book3.getYearPublishing(2010);
+        book1.setAutorBook("Иванов И. И.");
+        book1.setName("Приключения");
+        book1.setYearPublishing(2000);
+        book2.setAutorBook("Сидоров А. В");
+        book2.setName("Словарь");
+        book2.setYearPublishing(1980);
+        book3.setAutorBook("Гусев К. В.");
+        book3.setName("Энциклопедия");
+        book3.setYearPublishing(2010);
 System.out.println("FIO - " + reader.fio);
 
     }
