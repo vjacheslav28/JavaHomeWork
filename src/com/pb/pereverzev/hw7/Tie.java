52ac32627ee33bf36e88b23a3e5c082a5dbaac95
vjacheslav28 <perever.vjachesla@gmail.com>
2021-11-15 08:43:37 +0200

    init commit

--- src/com/pb/pereverzev/hw7/Tie.java
+++ src/com/pb/pereverzev/hw7/Tie.java
@@ -1,5 +1,8 @@
 package com.pb.pereverzev.hw7;
-
+/*
+Создать классы наследники Clothes:
+- Tie (галстук) реализует интерфейс ManClothes.
+ */
 public abstract  class Tie extends Clothes implements ManClothes{
 
     public Tie(Size clotherSize,int clothesPrice,String clothesColour){
